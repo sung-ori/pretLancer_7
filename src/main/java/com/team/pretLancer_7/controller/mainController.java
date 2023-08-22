03ebba78caae0d8ff22b81bfcec60d89a663fe8b
Fladion <ghkdua989@gmail.com>
2023-08-22 16:29:28 +0900

    연습중

--- src/main/java/com/team/pretLancer_7/controller/mainController.java
+++ src/main/java/com/team/pretLancer_7/controller/mainController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class mainController {
 
-    @GetMapping("/mainForm")
+    @GetMapping({"", "/"})
     public String mainForm() {
-        return "/mainForm/index.html";
+        return "main";
     }
     
     
