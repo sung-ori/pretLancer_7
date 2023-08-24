9ba7da0846677c82a75e1957d25b38522621b22d
sung_ori <l9707so@gmail.com>
2023-08-24 16:47:35 +0900

    password encoder

--- src/main/java/com/team/pretLancer_7/controller/MypageController.java
+++ src/main/java/com/team/pretLancer_7/controller/MypageController.java
@@ -2,21 +2,26 @@ package com.team.pretLancer_7.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.team.pretLancer_7.service.MypageService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
+@RequestMapping("my_page")
 public class MypageController {
 
     @Autowired
+    MypageService service;
 
-    @GetMapping("my_page")
-    public String myPageForm() {
+    // @GetMapping({" ","/"})
+    // public String myPageForm(Model m) {
+
 
         
-        return"/myPageView/MyPage";
-    }
+    //     return"/myPageView/MyPage";
+    // }
 
 }
--- src/main/java/com/team/pretLancer_7/controller/examController.java
+++ src/main/java/com/team/pretLancer_7/controller/examController.java
@@ -1,24 +1,23 @@
 package com.team.pretLancer_7.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("translated")
 public class examController {
 
-	@GetMapping("exam")
-	public String exam () {
+	// @GetMapping("exam")
+	// public String exam () {
 		
-		return "examForm/exam";
-	}
+	// 	return "examForm/exam";
+	// }
 	
-	@GetMapping("exam")
-	public String examAnswer () {
+	// @GetMapping("exam")
+	// public String examAnswer () {
 		
-		return "examForm/exam";
-	}
+	// 	return "examForm/exam";
+	// }
 	
 	
 }
