6f0b703bb08b7b033953db81120b9d1bd16cb43c
Fladion <ghkdua989@gmail.com>
2023-08-24 14:44:37 +0900

    회원가입 및 닉네임 중복체크 구현

--- src/main/java/com/team/pretLancer_7/service/MemberSerciceImple.java
+++ src/main/java/com/team/pretLancer_7/service/MemberSerciceImple.java
@@ -1,5 +1,27 @@
 package com.team.pretLancer_7.service;
 
-public class MemberSerciceImple implements MemberService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.team.pretLancer_7.dao.MemberDAO;
+import com.team.pretLancer_7.domain.Member;
+
+@Service
+public class MemberSerciceImple implements MemberService {
+	
+	@Autowired
+	MemberDAO dao;
+
+	@Override
+	public int insertMember(Member m) {
+		return dao.insertOne(m);
+	}
+
+	@Override
+	public boolean idcheck(String searchid) {
+		return dao.selectOne(searchid) == null;
+	}
+
+	
+	
 }
--- src/main/java/com/team/pretLancer_7/service/MemberService.java
+++ src/main/java/com/team/pretLancer_7/service/MemberService.java
@@ -1,6 +1,11 @@
 package com.team.pretLancer_7.service;
 
+import com.team.pretLancer_7.domain.Member;
+
 public interface MemberService {
 
-	
+	int insertMember(Member m);
+
+	boolean idcheck(String searchid);
+
 }
