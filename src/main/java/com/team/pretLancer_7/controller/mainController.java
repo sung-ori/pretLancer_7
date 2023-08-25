c81016ac7777a8d51e0a8258c8d49fc2f637f4f7
sung_ori <l9707so@gmail.com>
2023-08-25 16:38:37 +0900

    zzz

--- src/main/java/com/team/pretLancer_7/controller/mainController.java
+++ src/main/java/com/team/pretLancer_7/controller/mainController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class mainController {
     // practice.git
+    //iklasdjf;lkadsf;lkasdf
+    //adsjfl'oasdjf;lkasdjfk;la./ads;lkfjads;lfk//adsfasd///aksdlfj
+    //as;dklja;dlskj;lkasdfkl;adjsf;
     @GetMapping({" ", "/"})
     public String mainForm() {
         return "main";
