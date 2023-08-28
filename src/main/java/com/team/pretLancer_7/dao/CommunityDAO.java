6527b442cc001a37c46abf23aabdbfac778ad976
sung_ori <l9707so@gmail.com>
2023-08-28 17:46:28 +0900

    complete email

--- src/main/java/com/team/pretLancer_7/dao/CommunityDAO.java
+++ src/main/java/com/team/pretLancer_7/dao/CommunityDAO.java
@@ -2,8 +2,11 @@ package com.team.pretLancer_7.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.team.pretLancer_7.domain.Board;
 
+@Mapper
 public interface CommunityDAO {
     
     public List<Board> selectAll();
