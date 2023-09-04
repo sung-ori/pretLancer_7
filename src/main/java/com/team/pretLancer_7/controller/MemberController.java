734576c72d8d6116eee985e79ec58eda5091f825
kyoungjin1020 <kyoungjin1020@gmail.com>
2023-09-04 16:44:05 +0900

    no message

--- src/main/java/com/team/pretLancer_7/controller/MemberController.java
+++ src/main/java/com/team/pretLancer_7/controller/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.team.pretLancer_7.domain.Ability;
