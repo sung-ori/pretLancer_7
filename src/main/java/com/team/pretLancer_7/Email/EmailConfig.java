d1cfa8ec03fd2a6e9a67f21a80c898f616851e49
sung_ori <l9707so@gmail.com>
2023-09-06 14:24:41 +0900

    Force overwrite of EmailConfig.java

--- src/main/java/com/team/pretLancer_7/Email/EmailConfig.java
+++ src/main/java/com/team/pretLancer_7/Email/EmailConfig.java
@@ -9,7 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class EmailConfig {
-
+ 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
