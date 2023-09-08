82cfb63a63c44dbe8d9b36a801644d9bec0109a9
sung_ori <l9707so@gmail.com>
2023-09-08 15:29:50 +0900

    save

---
+++ src/main/java/com/team/pretLancer_7/websocket/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.team.pretLancer_7.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+    
+    @Override
+    public void registerStompEndpoints(final StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket") // 연결 시작점
+                .setHandshakeHandler(new UserHandshakeHandler())
+                .setAllowedOriginpatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(final MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub"); // 3번 우체통
+        registry.setApplicationDestinationPrezixes("/pub"); // 2번 집배원
+    }
+}
