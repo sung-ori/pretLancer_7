24da248d27bce1051f4ddb0a9545fe04d44441fa
sung_ori <l9707so@gmail.com>
2023-09-17 19:53:28 +0900

    save

--- src/main/java/com/team/pretLancer_7/domain/Message.java
+++ src/main/java/com/team/pretLancer_7/domain/Message.java
@@ -14,7 +14,6 @@ public class Message {
     public String message; // 실제 메세지 내용으로 화면에 띄워줄 내용
     public String checked; // 알림 칸을 확인했는지 여부
     public String click; // 클릭해서 알림을 실제 확인 했는지 여부
-
-
+    public String args; // 링크 열 때 인자가 되는 요소
 
 }
